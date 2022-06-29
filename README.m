1374dd9e7c56be36a148051aa11684d6a6f78c92
jrgdashi <106614247+jrgdashi@users.noreply.github.com>
2022-06-29 08:39:22 -0400

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,4 @@
+# Final
+Final project
+
+The final project it's gonna use the same data from the second project, the differencce is instead of looking at eveything I'm gonna look at the most recent games, basically when being a gamer became a viable option, work with the categorical data that I didn't use in project 2, and clean up the data
